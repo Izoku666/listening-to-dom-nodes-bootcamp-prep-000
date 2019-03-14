096bcb3a83036e00bf8d65e13fbe024ba6f781c7
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-14 23:28:56 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+// heads up guys, the first server link I got was unsecure and blocked by chrome.  after refreshing it, i was able to edit as needed
+
+
